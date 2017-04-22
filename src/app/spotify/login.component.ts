13e4984aa87b4dc21db4e92633c682ccdf628fc2
Estuardo Diaz <esjoando555@gmail.com>
2017-04-22 15:07:56 -0600

    Refactors login button

--- src/app/spotify/login.component.ts
+++
@@ -1,9 +0,0 @@
-import { BrowserDomAdapter } from 'angular2/platform/browser';
-
-export default class PostMessageService {
-  dom = new BrowserDomAdapter();
-
-  addPostMessageListener(fn: EventListener) : void {
-    this.dom.getGlobalEventTarget('window').addEventListener('message', fn, false);
-  }
-}
