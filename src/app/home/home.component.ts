90e5947b190803de14dd95ea5f6d9c04402c90c7
Estuardo Diaz <esjoando555@gmail.com>
2017-04-22 18:29:10 -0600

    Shows button if no user exists

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'home',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   title = 'We are building an awesome app, come back soon!';
 
-  constructor() { }
+  constructor(public auth : AuthService ) { }
 
   ngOnInit() {
   }
